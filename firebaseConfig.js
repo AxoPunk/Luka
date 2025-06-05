9f9e79e88e0e47b7d0d7ab903e5653678ba08baf
ItielSanz <113627289+ItielSanzAXO@users.noreply.github.com>
2025-06-04 23:57:21 -0600

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDFCFT5kYJH8GmTqDbHI_fLAFG7X2ZBRYE',
-  authDomain: 'mindcareia-cfdc2.firebaseapp.com',
-  projectId: 'mindcareia-cfdc2',
-  storageBucket: 'mindcareia-cfdc2.firebasestorage.app',
-  messagingSenderId: '720356584620',
-  appId: '1:720356584620:web:7ed39c2679d7eb923f4309',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
